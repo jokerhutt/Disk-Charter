8641216a71b44fb510851832aecc5058a5789e01
Dave <davidglogowski2003@gmail.com>
2025-07-27 21:45:34 +0200

    Initial Commit

---
+++ CleanDiff/CleanDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanDiffApp.swift
+//  CleanDiff
+//
+//  Created by David Glogowski on 27/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanDiffTests/CleanDiffTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanDiffTests.swift
+//  CleanDiffTests
+//
+//  Created by David Glogowski on 27/07/2025.
+//
+
+import Testing
+@testable import CleanDiff
+
+struct CleanDiffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
